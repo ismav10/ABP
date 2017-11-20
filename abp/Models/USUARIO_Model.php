d6beaa2a6fe456a0f6fd4ca6468afb034d764891
ivanddios <ivanddf1994@gmail.com>
2017-11-20 14:39:57 +0100

    bd

--- abp/Models/USUARIO_Model.php
+++ abp/Models/USUARIO_Model.php
@@ -42,7 +42,7 @@ class USUARIO_Modelo {
 //Función para conectarnos a la Base de datos
     function ConectarBD() {
         $this->mysqli = new mysqli("localhost", "root", "", "gymgest");
-
+        mysql_set_charset("utf8");
         if ($this->mysqli->connect_errno) {
             echo "Fallo al conectar a MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error;
         }
