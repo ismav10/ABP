a41ac9201ba638af3a87deacc819efbbf42e3e7a
apframinhan <apframinhan@esei.uvigo.es>
2017-12-25 19:54:52 +0100

    Add files via upload

---
+++ abp/Controllers/ESTADISTICAS_Controller.php
@@ -0,0 +1,36 @@
+<?php
+
+//Controlador para la gestión de usuarios
+include '../Models/SESION_Model.php';
+include '../Views/MENSAJE_Vista.php';
+
+
+if (!IsAuthenticated()) {
+    header('Location:../index.php');
+}
+include '../Views/header.php';
+include '../Locates/Strings_' . $_SESSION['IDIOMA'] . '.php';
+if (!isset($_REQUEST['accion'])) {
+    $_REQUEST['accion'] = '';
+}
+
+$pags = generarIncludes(); //Realizamos los includes de las páginas a las que tiene acceso
+for ($z = 0; $z < count($pags); $z++) {
+    include $pags[$z];
+}
+
+Switch ($_REQUEST['accion'])
+{ //Actúa según la acción elegida
+
+    
+    default:
+        //if (!tienePermisos('SESION_Listar')) {
+            //new Mensaje('No tienes los permisos necesarios', '../Views/DEFAULT_Vista.php');
+        //} else {
+            $sesion = new SESION_Model($_SESSION['login'], '', '', '', '', '', '', '');
+            $datos = "";
+			require_once '../Views/ESTADISTICAS_SHOWALL.php';
+            new ESTADISTICAS_SHOWALL($datos, 'SESION_Controller.php');
+        //}
+}
+?>
--- abp/Controllers/TABLA_Controller.php
+++ abp/Controllers/TABLA_Controller.php
@@ -68,15 +68,53 @@ switch ($_REQUEST['accion']) { //Actúa según la acción elegida
             new TABLA_ShowCurrent($datos, '../Views/EJERCICIO_SHOWALL_Vista.php');
         }
         break;
+		
+	case 'frmasignar':
+		//FALTA DAR PERMISOS
+		//if (!tienePermisos('TABLA_ASIGN')) {
+        //    new Mensaje('No tienes los permisos necesarios', '../Views/DEFAULT_Vista.php');
+        //} else {
+            require_once '../Views/TABLA_ASIGN_Vista.php';
+			$tabla = get_data_form();
+            $ejercicios = new EJERCICIO_Model("", "", "");
+			$ejer = $ejercicios->obtenerEjercicios();
+			$datos['selectejercicios'] = "";
+			foreach ($ejer as &$valor)
+			{
+				$datos['selectejercicios'] .= "<option>".$valor['nombreEjercicio']."</option>";
+			}
+            
+            new TABLA_ASIGN($datos, '../Views/TABLA_SHOWALL_Vista.php');
+        //}
+	break;
 
     case 'asignar':
-            $tabla = get_data_form();
-            $tabla->asignarEjercicios($_REQUEST['id'], $_POST['asignacionEjercicios']);
-			echo '<script> location.replace("../Controllers/TABLA_Controller.php"); </script>';
+			
+			//Recorremos todas las filas de 4 en 4
+			$asign_data = $_POST;
+			$tabla = get_data_form();
+			$asign_data_formated = array_chunk($asign_data, 4, false);
+			foreach ($asign_data_formated as $key => $value)
+			{
+				//Una vez tenemos cada seccion del array
+				//Guardamos la asignacion en la bd con sus correspondientes datos de forma que
+				// Orden del Array
+				// 1 - Nombre de Ejercicio
+			    // 2 - NumRepeticiones
+				// 3 - Num. Series
+				// 4 - Duracion
+				$tabla->asignarEjercicios($_REQUEST['id'], $value);
+			}
+			echo '<script> location.replace("../Controllers/TABLA_Controller.php?accion=ver&id='.$_REQUEST['id'].'"); </script>';
 			exit(0);
-        
         break;
-
+		
+	case 'desasignar':
+			$tabla = get_data_form();
+            $tabla->desasignarEjercicio($_REQUEST['idtabla'], $_REQUEST['idejercicio']);
+			echo '<script> location.replace("../Controllers/TABLA_Controller.php?accion=ver&id='.$_REQUEST['idtabla'].'"); </script>';
+			exit(0);
+	break;
     case 'insertar':
         if (!tienePermisos('TABLA_ADD')) {
             new Mensaje('No tienes los permisos necesarios', '../Views/DEFAULT_Vista.php');
