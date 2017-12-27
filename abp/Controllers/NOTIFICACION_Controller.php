032ac1a30f9e26e1b13ec81c97ddd1cc17a2767c
ismav10 <ismavazquez@alumnos.uvigo.es>
2017-12-28 00:20:38 +0100

    Add files via upload

--- abp/Controllers/NOTIFICACION_Controller.php
+++ abp/Controllers/NOTIFICACION_Controller.php
@@ -41,11 +41,25 @@ switch ($_REQUEST['accion']) {
             new Mensaje('No tienes los permisos necesarios', '../Views/DEFAULT_Vista.php');
         } else {
             if (!isset($_REQUEST['username'])) {
-                new NOTIFICACION_Insertar('NOTIFICACION_Controller.php');
+                //Si es administrador le pasa todos los usuarios a la vista
+                if(ConsultarTipoUsuario($_SESSION['login'])==1)
+                {
+                    $notificacion = new NOTIFICACION_Model('','','','','','',$_SESSION['login']);
+                    $datos = $notificacion->ConsultarUsuarios();
+                new NOTIFICACION_Insertar($datos,'../Controllers/NOTIFICACION_Controller.php');
+                }
+                //Si es entrenador le pasa solo los deportistas a la vista
+                else 
+                    if(ConsultarTipoUsuario($_SESSION['login'])==2)
+                    {
+                    $notificacion = new NOTIFICACION_Model('','','','','','',$_SESSION['login']);
+                    $datos = $notificacion->ConsultarDeportistas();
+                        new NOTIFICACION_Insertar($datos,'../Controllers/NOTIFICACION_Controller.php');
+                    }            
             } else {
                 $notificacion = new NOTIFICACION_Model('', '', $_REQUEST['destinatarioNotificacion'], '', $_REQUEST['asuntoNotificacion'], $_REQUEST['mensajeNotificacion'], $_SESSION['login']);
                 $respuesta = $notificacion->Insertar();
-                new Mensaje($respuesta, '../Views/NOTIFICACION_SHOWALL_Vista.php');
+                new Mensaje($respuesta, '../Controllers/NOTIFICACION_Controller.php');
             }
         }
         break;
@@ -62,28 +76,29 @@ switch ($_REQUEST['accion']) {
         } else {
             $notificacion = new NOTIFICACION_Model($_REQUEST['identificador'], '', '', '', '', '', '');
             $respuesta = $notificacion->Borrar();
-            new Mensaje($respuesta, '../Views/NOTIFICACION_SHOWALL_Vista.php');
+            new Mensaje($respuesta, '../Controllers/NOTIFICACION_Controller.php');
         }
         break;
 
     case $strings['Consultar']:
-        if (!isset($_REQUEST['usuarioConsulta'])) {
+        if (!isset($_REQUEST['consulta'])) {
             if (!tienePermisos('NOTIFICACION_Consultar')) {
                 new Mensaje('No tienes los permisos necesarios', '../Views/DEFAULT_Vista.php');
             } else {
-                new NOTIFICACION_Consultar('../Views/NOTIFICACION_SHOWALL_Vista.php');
+                new NOTIFICACION_Consultar('../Controllers/NOTIFICACION_Controller.php');
             }
         } else {
-            $notificacion = new NOTIFICACION_Model('', $_REQUEST['remitente'], $_REQUEST['destinatario'], $_REQUEST['fechaHoraNotificacion'], $_REQUEST['asunto'], '', $_REQUEST['username']);
+            $notificacion = new NOTIFICACION_Model('',$_REQUEST['Remitente'],'','',$_REQUEST['Asunto'],'',$_SESSION['login']);
             $datos = $notificacion->Consultar();
-            new NOTIFICACION_Consulta($datos, '../Views/NOTIFICACION_SHOWALL_Vista.php');
+            $tipoUsuario = ConsultarTipoUsuario($_SESSION['login']);
+            new NOTIFICACION_Listar($datos, $tipoUsuario, '../Controllers/NOTIFICACION_Controller.php');
         }
 
     //En el caso de que quiera consultar una notificacion en concreto creo la notificacion solo con el id que va a venir de la vista,
     //llamo a la funcion que obtiene los datos sobre esa notificacion y creo una vista showcurrent mostrando los datos y con el controlador de las notificaciones.
     case $strings['Ver']:
         if (!tienePermisos('NOTIFICACION_Seleccionar')) {
-            new Mensaje('No tienes los permisos necesarios', '../Views/DEFAULT_Vista.php');
+            new Mensaje('No tienes los permisos necesarios', '../Controllers/NOTIFICACION_Controller.php');
         } else {
             if (!isset($_REQUEST['idNotificacion'])) {
                 
@@ -91,7 +106,7 @@ switch ($_REQUEST['accion']) {
                 $idNotificacion = $_REQUEST['idNotificacion'];
                 $notificacion = new NOTIFICACION_Model($idNotificacion, '', '', '', '', '', '');
                 $resultado = $notificacion->RellenaDatos();
-                new NOTIFICACION_Seleccionar($resultado, '../Views/NOTIFICACION_SHOWALL_Vista.php');
+                new NOTIFICACION_Seleccionar($resultado, '../Controllers/NOTIFICACION_Controller.php');
             }
         }
         break;
@@ -105,7 +120,7 @@ switch ($_REQUEST['accion']) {
             $notificacion = new NOTIFICACION_Model('', '', '', '', '', '', $_SESSION['login']);
             $datos = $notificacion->Listar();
             $tipoUsuario = ConsultarTipoUsuario($_SESSION['login']);
-            new NOTIFICACION_Listar($datos, $tipoUsuario, '../Views/NOTIFICACION_SHOWALL_Vista.php');
+            new NOTIFICACION_Listar($datos, $tipoUsuario, '../Controllers/NOTIFICACION_Controller.php');
         }
 }
 ?>
