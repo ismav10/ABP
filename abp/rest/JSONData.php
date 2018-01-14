628ed8674331e7cfe2243d597edd7eb290a3413b
apframinhan <apframinhan@esei.uvigo.es>
2018-01-14 12:02:13 +0100

    Solucionado ERROR para JSON.Encode

--- abp/rest/JSONData.php
+++ abp/rest/JSONData.php
@@ -7,8 +7,24 @@ session_start();
 $sesion = new SESION_Model($_SESSION['login'], '', '', '', '', '', '', '');
 //$sesion = new SESION_Model('deportista1', '', '', '', '', '', '', '');
 $sesiones = $sesion->Listar();
+
+$sesiones_array = array();
+
+foreach($sesiones as $sesion)
+{
+				//print_r($sesion["idTabla"]);
+				array_push($sesiones_array, array("idTabla" => $sesion["idTabla"],
+												"horaInicio" => $sesion[5],
+												"horaFin" => $sesion[6],
+											 ));
+											 
+											 
+}
+			
+
 header('Content-Type: application/json');
-echo json_encode($sesiones);
+echo json_encode($sesiones_array);
+header($_SERVER['SERVER_PROTOCOL'].' 200 Ok');
 exit(0);
 
 ?>
